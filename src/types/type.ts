ef9edaa4cb0a61ff2f68d555033ccc9aac85d612
yj-oh <holidayoh99@gmail.com>
2021-01-20 02:48:27 +0900

    [추가] types | type 생성

---
+++ src/types/type.ts
@@ -0,0 +1,11 @@
+export type articleProps = {
+	title?: string;
+	description?: string;
+	url?: string;
+	urlToImage?: string | null;
+};
+
+export interface Category {
+	name: string;
+	text: string;
+}
